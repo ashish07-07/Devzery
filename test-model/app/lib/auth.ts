ae6d57253c52dc83e1af6fab5ad74073d9321aa3
B <bkashishh07@gmail.com>
2024-07-26 13:19:40 +0530

    Update auth.ts

--- test-model/app/lib/auth.ts
+++ test-model/app/lib/auth.ts
@@ -1,15 +1,100 @@
+// import NextAuth from "next-auth/next";
+// import { NextRequest, NextResponse } from "next/server";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import bcrypt from "bcrypt";
+// import GitHubProvider from "next-auth/providers/github";
+
+// import prisma from "../db";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const NEXT_AUTH = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "Email",
+//       credentials: {
+//         email: {
+//           label: "email",
+//           type: "text",
+//           placeholder: "bkashishh07@gmail.com",
+//         },
+//         password: { label: "Password", type: "text", placeholder: "123456" },
+//       },
+//       async authorize(credentials: any) {
+//         const email = credentials.email;
+//         const password = credentials.password;
+//         const hashedPassword = await bcrypt.hash(password, 10);
+
+//         const existingUser = await prisma.user.findFirst({
+//           where: { email },
+//         });
+
+//         if (existingUser) {
+//           const passwordValidator = await bcrypt.compare(
+//             password,
+//             existingUser.password
+//           );
+
+//           if (passwordValidator) {
+//             return {
+//               id: existingUser.id.toString(),
+//               email: existingUser.email,
+//             };
+//           }
+//           return null;
+//         }
+
+//         try {
+//           const user = await prisma.user.create({
+//             data: {
+//               email,
+//               password: hashedPassword,
+//             },
+//           });
+
+//           return {
+//             id: user.id.toString(),
+
+//             email: user.email,
+//           };
+//         } catch (e) {}
+
+//         return null;
+//       },
+//     }),
+
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_ID || "",
+//       clientSecret: process.env.GOOGLE_SECRET || "",
+//     }),
+
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID || "",
+//       clientSecret: process.env.GITHUB_SECRET || "",
+//     }),
+//   ],
+
+//   secret: process.env.NEXTAUTH_SECRET,
+//   callbacks: {
+//     async session({ token, session }: any) {
+//       console.log(token);
+//       session.user.id = token.sub;
+//       //   session.role = "customer";
+
+//       return session;
+//     },
+//   },
+// };
+
 import NextAuth from "next-auth/next";
 import { NextRequest, NextResponse } from "next/server";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 import GitHubProvider from "next-auth/providers/github";
-
 import prisma from "../db";
 import GoogleProvider from "next-auth/providers/google";
 
 export const NEXT_AUTH = {
   providers: [
-    process.env.VERCE_ENV=="preview"?
     CredentialsProvider({
       name: "Email",
       credentials: {
@@ -54,10 +139,11 @@ export const NEXT_AUTH = {
 
           return {
             id: user.id.toString(),
-
             email: user.email,
           };
-        } catch (e) {}
+        } catch (e) {
+          console.error(e);
+        }
 
         return null;
       },
